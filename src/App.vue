e4f52710ad5520bcdd1b360d0f8a3243dfd1dd27
masudhajari <azharc614@gmail.com>
2020-10-25 00:19:22 +0600

    vue js cli project create $ ui design and inventory part daynamic

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <div class="container">
+        <div class="row">
+          <div class="col-sm-9 mt-2">
+              <Inventory :items="items"></Inventory>
+          </div>
+          <div class="col-sm-3 mt-2">
+              <Cart></Cart>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Inventory from './components/Inventory'
+import data from './data.js'
+
+export default {
+  components:{
+    Navbar,
+    Cart,
+    Inventory
+  },
+  data(){
+    return{
+      items: []
+    }
+  },
+  mounted(){
+    this.items = data
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
---
+++ src/components/Inventory.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row">
+        <div v-for="(item, index) in items" :key="index" class="card my-2 mx-2" style="width: 16rem;">
+            <img :src="item.photo" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ item.title }}</h5>
+                <p class="card-text">{{ item.price }}</p>
+                <a href="#" class="btn btn-primary">+ add</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['items']
+}
+</script>
+
+<style>
+
+</style>
